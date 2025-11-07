5e8d135f33d88550eb392d547720e93227ea8a18
Himanshu Verma <whoishimanshuverma@gmail.com>
2025-11-07 08:16:42 +0530

    Rename complete-profile.jsx to complete-profile.tsx

--- src/pages/tutor-flow/complete-profile.jsx
+++
@@ -1,5 +0,0 @@
-import CompleteProfile from "@/components/tutor-flow/pages/complete-profile";
-
-export default function CompleteProfilePage() {
-  return <CompleteProfile />;
-}
---
+++ src/pages/tutor-flow/complete-profile.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import CompleteProfile from "@/tutor-flow/complete-profile";
+
+const CompleteProfilePage: NextPage = () => {
+  return <CompleteProfile />;
+};
+
+export default CompleteProfilePage;
