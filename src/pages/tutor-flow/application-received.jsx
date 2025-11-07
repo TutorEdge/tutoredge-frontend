2df41151cd2bed2c722706244997aae562128715
Himanshu Verma <whoishimanshuverma@gmail.com>
2025-11-07 08:16:14 +0530

    Update and rename application-received.jsx to application-received.tsx

--- src/pages/tutor-flow/application-received.jsx
+++
@@ -1,5 +0,0 @@
-import ApplicationReceived from "@/components/tutor-flow/pages/application-received";
-
-export default function ApplicationReceivedPage() {
-  return <ApplicationReceived />;
-}
---
+++ src/pages/tutor-flow/application-received.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import ApplicationReceived from "@/tutor-flow/application-received";
+
+const ApplicationReceivedPage: NextPage = () => {
+  return <ApplicationReceived />;
+};
+
+export default ApplicationReceivedPage;
