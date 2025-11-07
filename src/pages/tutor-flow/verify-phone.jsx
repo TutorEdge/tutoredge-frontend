bc4d8df24a3fe461f478ca1ed40a00c6bef76fad
Himanshu Verma <whoishimanshuverma@gmail.com>
2025-11-07 08:15:46 +0530

    Update and rename verify-phone.jsx to verify-phone.tsx

--- src/pages/tutor-flow/verify-phone.jsx
+++
@@ -1,5 +0,0 @@
-import VerifyPhone from "@/components/tutor-flow/pages/verify-phone";
-
-export default function VerifyPhonePage() {
-  return <VerifyPhone />;
-}
---
+++ src/pages/tutor-flow/verify-phone.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import VerifyPhone from "@/tutor-flow/verify-phone";
+
+const VerifyPhonePage: NextPage = () => {
+  return <VerifyPhone />;
+};
+
+export default VerifyPhonePage;
